32b9ef9e0357ffaac3692900e836c4766765e0cf
GabLandier <gabin.landier@etu.cyu.fr>
2024-11-28 13:26:07 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,2 +1,22 @@
 #include <stdio.h>
-test
+
+typedef struct _tree{
+  int id;
+  struct _tree* pLeft;
+  struct _tree* pRight;
+  int balance;
+}Tree;
+
+typedef Tree* pTree;
+
+pTree createTree(int id){
+  pTree pNew=malloc(sizeof(Tree));
+  if (pNew==NULL){
+    exit(1);
+  }
+  pNew->pLeft=NULL;
+}
+
+int main(){
+    return 0;
+}
