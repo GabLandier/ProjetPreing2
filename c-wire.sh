ccb5b7b8a431419d414e90ba653fe9009c007af3
GabLandier <gabin.landier@etu.cyu.fr>
2024-12-09 09:45:54 +0100

    Shell

--- c-wire.sh
+++ c-wire.sh
@@ -39,6 +39,11 @@ else
 	mkdir graphs
 fi
 
+
+#Verfier que le fichier.csv existe bien
+
+
+
 if [[ "$#" == "3" ]] ; then
 	if [[ "$1" == "-h" || "$2" == "-h" || "$3" == "-h" ]] ; then
 		echo "Voila l'aide"
@@ -47,28 +52,34 @@ if [[ "$#" == "3" ]] ; then
 	
 			"hvb comp")
 				cat "input/$1" | cut -d ';' -f 1,2,3,5,7,8 | tail -n +2 | tr '-' '0' | awk '{printf("%s;1\n", $0);}' | codeC/prg > temp/fichier_temp.csv
-				cat temp/fichier_temp.csv | sort -t ':' -k 2,2n > tests/hvb_comp.csv
+				echo "Station HV-B : Capacité : Consommation (entreprise)" > tests/hvb_comp.csv
+				cat temp/fichier_temp.csv | sort -t ':' -k 2,2n >> tests/hvb_comp.csv
 				;;
 			"hva comp")
 				cat "input/$1" | cut -d ';' -f 1,3,4,5,7,8 | tail -n +2 | tr '-' '0' | awk '{printf("%s;1\n", $0);}' | codeC/prg > temp/fichier_temp.csv
-				cat temp/fichier_temp.csv | sort -t ':' -k 2,2n > tests/hva_comp.csv
+				echo "Station HV-A : Capacité : Consommation (entreprise)" > tests/hva_comp.csv
+				cat temp/fichier_temp.csv | sort -t ':' -k 2,2n >> tests/hva_comp.csv
 				;;
 			"lv comp")
 				cat "input/$1" | cut -d ';' -f 1,4,5,6,7,8 | tail -n +2 | tr '-' '0' | awk '{printf("%s;2\n", $0);}' | codeC/prg > temp/fichier_temp.csv
-				cat temp/fichier_temp.csv | sort -t ':' -k 2,2n > tests/lv_comp.csv
+				echo "Station LV : Capacité : Consommation (entreprise)" > tests/lv_comp.csv
+				cat temp/fichier_temp.csv | sort -t ':' -k 2,2n >> tests/lv_comp.csv
 				;;
 			"lv indiv")
 				cat "input/$1" | cut -d ';' -f 1,4,5,6,7,8 | tail -n +2 | tr '-' '0' | awk '{printf("%s;3\n", $0);}' | codeC/prg > temp/fichier_temp.csv
-				cat temp/fichier_temp.csv | sort -t ':' -k 2,2n > tests/lv_indiv.csv
+				echo "Station LV : Capacité : Consommation (particuliers)" > tests/lv_indiv.csv
+				cat temp/fichier_temp.csv | sort -t ':' -k 2,2n >> tests/lv_indiv.csv
 				;;
 			"lv all")
 				cat "input/$1" | cut -d ';' -f 1,4,5,6,7,8 | tail -n +2 | tr '-' '0' | awk '{printf("%s;4\n", $0);}' | codeC/prg > temp/fichier_temp.csv
-				cat temp/fichier_temp.csv | sort -t ':' -k 2,2n > tests/lv_all.csv
+				echo "Station LV : Capacité : Consommation" > tests/lv_all.csv
+				cat temp/fichier_temp.csv | sort -t ':' -k 2,2n >> tests/lv_all.csv
+				echo "Station LV : Capacité : Consommation" > tests/lv_all_minmax.csv
 				if [[ $(wc -l < temp/fichier_temp.csv) -gt 20 ]] ; then
-					cat temp/fichier_temp.csv | sort -t ':' -k 2,2nr | head -n10 > tests/lv_all_minmax.csv
+					cat temp/fichier_temp.csv | sort -t ':' -k 2,2nr | head -n10 >> tests/lv_all_minmax.csv
 					cat temp/fichier_temp.csv | sort -t ':' -k 2,2nr | tail -n10 >> tests/lv_all_minmax.csv
 				else
-					cat temp/fichier_temp.csv | sort -t ':' -k 2,2nr > tests/lv_all_minmax.csv
+					cat temp/fichier_temp.csv | sort -t ':' -k 2,2nr >> tests/lv_all_minmax.csv
 				fi
 				;;
 			*)
@@ -85,28 +96,34 @@ elif [[ "$#" == "4" ]] ; then
 	
 			"hvb comp")
 				cat "input/$1" | cut -d ';' -f 1,2,3,5,7,8 | tail -n +2 | tr '-' '0' | awk '{printf("%s;1\n", $0);}' | awk -F';' -v col4="$4" '$1==col4' | codeC/prg > temp/fichier_temp.csv
-				cat temp/fichier_temp.csv | sort -t ':' -k 2,2n > tests/hvb_comp_$4.csv
+				echo "Station HV-B : Capacité : Consommation (entreprise)" > tests/hvb_comp_$4.csv
+				cat temp/fichier_temp.csv | sort -t ':' -k 2,2n >> tests/hvb_comp_$4.csv
 				;;
 			"hva comp")
 				cat "input/$1" | cut -d ';' -f 1,3,4,5,7,8 | tail -n +2 | tr '-' '0' | awk '{printf("%s;1\n", $0);}' | awk -F';' -v col4="$4" '$1==col4' | codeC/prg > temp/fichier_temp.csv
-				cat temp/fichier_temp.csv | sort -t ':' -k 2,2n > tests/hva_comp_$4.csv
+				echo "Station HV-A : Capacité : Consommation (entreprise)" > tests/hva_comp_$4.csv
+				cat temp/fichier_temp.csv | sort -t ':' -k 2,2n >> tests/hva_comp_$4.csv
 				;;
 			"lv comp")
 				cat "input/$1" | cut -d ';' -f 1,4,5,6,7,8 | tail -n +2 | tr '-' '0' | awk '{printf("%s;2\n", $0);}' | awk -F';' -v col4="$4" '$1==col4' | codeC/prg > temp/fichier_temp.csv
-				cat temp/fichier_temp.csv | sort -t ':' -k 2,2n > tests/lv_comp_$4.csv
+				echo "Station LV : Capacité : Consommation (entreprise)" > tests/lv_comp_$4.csv
+				cat temp/fichier_temp.csv | sort -t ':' -k 2,2n >> tests/lv_comp_$4.csv
 				;;
 			"lv indiv")
 				cat "input/$1" | cut -d ';' -f 1,4,5,6,7,8 | tail -n +2 | tr '-' '0' | awk '{printf("%s;3\n", $0);}' | awk -F';' -v col4="$4" '$1==col4' | codeC/prg > temp/fichier_temp.csv
-				cat temp/fichier_temp.csv | sort -t ':' -k 2,2n > tests/lv_indiv_$4.csv
+				echo "Station LV : Capacité : Consommation (particuliers)" > tests/lv_indiv_$4.csv
+				cat temp/fichier_temp.csv | sort -t ':' -k 2,2n >> tests/lv_indiv_$4.csv
 				;;
 			"lv all")
 				cat "input/$1" | cut -d ';' -f 1,4,5,6,7,8 | tail -n +2 | tr '-' '0' | awk '{printf("%s;4\n", $0);}' | awk -F';' -v col4="$4" '$1==col4' | codeC/prg > temp/fichier_temp.csv
-				cat temp/fichier_temp.csv | sort -t ':' -k 2,2n > tests/lv_all_$4.csv
+				echo "Station LV : Capacité : Consommation" > tests/lv_all_$4.csv
+				cat temp/fichier_temp.csv | sort -t ':' -k 2,2n >> tests/lv_all_$4.csv
+				echo "Station LV : Capacité : Consommation" > tests/lv_all_minmax_$4.csv
 				if [[ $(wc -l < temp/fichier_temp.csv) -gt 20 ]] ; then
-					cat temp/fichier_temp.csv | sort -t ':' -k 2,2nr | head -n10 > tests/lv_all_minmax_$4.csv
+					cat temp/fichier_temp.csv | sort -t ':' -k 2,2nr | head -n10 >> tests/lv_all_minmax_$4.csv
 					cat temp/fichier_temp.csv | sort -t ':' -k 2,2nr | tail -n10 >> tests/lv_all_minmax_$4.csv
 				else
-					cat temp/fichier_temp.csv | sort -t ':' -k 2,2nr > tests/lv_all_minmax_$4.csv
+					cat temp/fichier_temp.csv | sort -t ':' -k 2,2nr >> tests/lv_all_minmax_$4.csv
 				fi
 				;;
 			*)
@@ -126,28 +143,34 @@ elif [[ "$#" == "5" ]] ; then
 	
 			"hvb comp")
 				cat "input/$1" | cut -d ';' -f 1,2,3,5,7,8 | tail -n +2 | tr '-' '0' | awk '{printf("%s;1\n", $0);}' | awk -F';' -v col4="$4" '$1==col4' | codeC/prg > temp/fichier_temp.csv
-				cat temp/fichier_temp.csv | sort -t ':' -k 2,2n > tests/hvb_comp_$4.csv
+				echo "Station HV-B : Capacité : Consommation (entreprise)" > tests/hvb_comp_$4.csv
+				cat temp/fichier_temp.csv | sort -t ':' -k 2,2n >> tests/hvb_comp_$4.csv
 				;;
 			"hva comp")
 				cat "input/$1" | cut -d ';' -f 1,3,4,5,7,8 | tail -n +2 | tr '-' '0' | awk '{printf("%s;1\n", $0);}' | awk -F';' -v col4="$4" '$1==col4' | codeC/prg > temp/fichier_temp.csv
-				cat temp/fichier_temp.csv | sort -t ':' -k 2,2n > tests/hva_comp_$4.csv
+				echo "Station HV-A : Capacité : Consommation (entreprise)" > tests/hva_comp_$4.csv
+				cat temp/fichier_temp.csv | sort -t ':' -k 2,2n >> tests/hva_comp_$4.csv
 				;;
 			"lv comp")
 				cat "input/$1" | cut -d ';' -f 1,4,5,6,7,8 | tail -n +2 | tr '-' '0' | awk '{printf("%s;2\n", $0);}' | awk -F';' -v col4="$4" '$1==col4' | codeC/prg > temp/fichier_temp.csv
-				cat temp/fichier_temp.csv | sort -t ':' -k 2,2n > tests/lv_comp_$4.csv
+				echo "Station LV : Capacité : Consommation (entreprise)" > tests/lv_comp_$4.csv
+				cat temp/fichier_temp.csv | sort -t ':' -k 2,2n >> tests/lv_comp_$4.csv
 				;;
 			"lv indiv")
 				cat "input/$1" | cut -d ';' -f 1,4,5,6,7,8 | tail -n +2 | tr '-' '0' | awk '{printf("%s;3\n", $0);}' | awk -F';' -v col4="$4" '$1==col4' | codeC/prg > temp/fichier_temp.csv
-				cat temp/fichier_temp.csv | sort -t ':' -k 2,2n > tests/lv_indiv_$4.csv
+				echo "Station LV : Capacité : Consommation (particuliers)" > tests/lv_indiv_$4.csv
+				cat temp/fichier_temp.csv | sort -t ':' -k 2,2n >> tests/lv_indiv_$4.csv
 				;;
 			"lv all")
 				cat "input/$1" | cut -d ';' -f 1,4,5,6,7,8 | tail -n +2 | tr '-' '0' | awk '{printf("%s;4\n", $0);}' | awk -F';' -v col4="$4" '$1==col4' | codeC/prg > temp/fichier_temp.csv
-				cat temp/fichier_temp.csv | sort -t ':' -k 2,2n > tests/lv_all_$4.csv
+				echo "Station LV : Capacité : Consommation" > tests/lv_all_$4.csv
+				cat temp/fichier_temp.csv | sort -t ':' -k 2,2n >> tests/lv_all_$4.csv
+				echo "Station LV : Capacité : Consommation" > tests/lv_all_minmax_$4.csv
 				if [[ $(wc -l < temp/fichier_temp.csv) -gt 20 ]] ; then
-					cat temp/fichier_temp.csv | sort -t ':' -k 2,2nr | head -n10 > tests/lv_all_minmax_$4.csv
+					cat temp/fichier_temp.csv | sort -t ':' -k 2,2nr | head -n10 >> tests/lv_all_minmax_$4.csv
 					cat temp/fichier_temp.csv | sort -t ':' -k 2,2nr | tail -n10 >> tests/lv_all_minmax_$4.csv
 				else
-					cat temp/fichier_temp.csv | sort -t ':' -k 2,2nr > tests/lv_all_minmax_$4.csv
+					cat temp/fichier_temp.csv | sort -t ':' -k 2,2nr >> tests/lv_all_minmax_$4.csv
 				fi
 				;;
 			*)
